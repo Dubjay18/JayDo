0e9e502b3ac35f477c00b042faa78d061d016aa1
Dubjay18 <jejeniyi7@gmail.com>
2023-07-25 21:54:43 +0100

    feat: initialized firebase

---
+++ firebase.ts
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyCu6xHEX1GyhKpacUdq1KBilro3_tuOtow",
+  authDomain: "next-fb-eee87.firebaseapp.com",
+  projectId: "next-fb-eee87",
+  storageBucket: "next-fb-eee87.appspot.com",
+  messagingSenderId: "426669847165",
+  appId: "1:426669847165:web:fb8d03e847fb3f5e9f3714",
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
